81954d182decde290399a78dfde08a4d33a1962c
martier <martier@oocl.com>
2021-08-03 23:56:27 +0800

    [Pos Machine][Eric] refactor: updated Item.java

--- src/main/java/pos/machine/Item.java
+++ src/main/java/pos/machine/Item.java
@@ -1,10 +1,11 @@
 package pos.machine;
 
+
 public class Item {
-    private final String name;
-    private final int quantity;
-    private final int unitPrice;
-    private final int subTotal;
+    private String name;
+    private int quantity;
+    private int unitPrice;
+    private int subTotal;
 
     public Item(String name, int quantity, int unitPrice, int subTotal) {
         this.name = name;
@@ -13,19 +14,39 @@ public class Item {
         this.subTotal = subTotal;
     }
 
+    public Item() {
+
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getQuantity() {
         return quantity;
     }
 
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
     public int getUnitPrice() {
         return unitPrice;
     }
 
+    public void setUnitPrice(int unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
     public int getSubTotal() {
         return subTotal;
+    }
+
+    public void setSubTotal(int subTotal) {
+        this.subTotal = subTotal;
     }
 }
