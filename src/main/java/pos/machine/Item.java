4bcbcc29acaf905c13659ae3e6a2fdcc3f2a77ad
martier <martier@oocl.com>
2021-08-03 23:00:39 +0800

    [Pos Machine][Eric] feat: add Item.java

---
+++ src/main/java/pos/machine/Item.java
@@ -0,0 +1,31 @@
+package pos.machine;
+
+public class Item {
+    private final String name;
+    private final int quantity;
+    private final int unitPrice;
+    private final int subTotal;
+
+    public Item(String name, int quantity, int unitPrice, int subTotal) {
+        this.name = name;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+        this.subTotal = subTotal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public int getSubTotal() {
+        return subTotal;
+    }
+}
