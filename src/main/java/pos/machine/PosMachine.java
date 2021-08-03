42250568f6768e5082d6d33b89c80d73a8816ae1
martier <martier@oocl.com>
2021-08-04 00:00:02 +0800

    [Pos Machine][Eric] feat: added convertToItems() in PosMachine.java

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,30 @@
 package pos.machine;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
+        List<Item> itemsWithDetail = convertToItems(barcodes);
         return null;
+    }
+
+    private List<Item> convertToItems(List<String> barcodes) {
+        List<ItemInfo> itemInfoList = ItemDataLoader.loadAllItemInfos();
+        List<Item> itemList = new ArrayList<>();
+        List<String> distinctBarcodes = barcodes.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        for (String barcode : distinctBarcodes) {
+            for (ItemInfo itemInfo : itemInfoList) {
+                if (barcode.equals(itemInfo.getBarcode())) {
+                    int quantity = Collections.frequency(barcodes, barcode);
+                    int price = itemInfo.getPrice();
+                    Item item = new Item(itemInfo.getName(), quantity, price, quantity * price);
+                    itemList.add(item);
+                }
+            }
+        }
+        return itemList;
     }
 }
