ffefff579112b3d939153389797303fd7710e1eb
martier <martier@oocl.com>
2021-08-04 00:11:40 +0800

    [Pos Machine][Eric] refactor: updated Receipt.java

--- src/main/java/pos/machine/Receipt.java
+++ src/main/java/pos/machine/Receipt.java
@@ -1,2 +1,29 @@
-package pos.machine;public class Receipt {
+package pos.machine;
+
+import java.util.List;
+
+public class Receipt {
+    private List<Item> itemDetail;
+    private int totalPrice;
+
+    public Receipt(List<Item> itemDetail, int totalPrice) {
+        this.itemDetail = itemDetail;
+        this.totalPrice = totalPrice;
+    }
+
+    public List<Item> getItemDetail() {
+        return itemDetail;
+    }
+
+    public void setItemDetail(List<Item> itemDetail) {
+        this.itemDetail = itemDetail;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
