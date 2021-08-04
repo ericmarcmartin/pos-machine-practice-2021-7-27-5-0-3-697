d114ddd3bb876ec4c23bae18bebb777ab58f4124
martier <martier@oocl.com>
2021-08-04 19:08:49 +0800

    Revert "[Pos Machine][Eric] test: updated PosMachineTest.java"

--- src/test/java/pos/machine/PosMachineTest.java
+++ src/test/java/pos/machine/PosMachineTest.java
@@ -11,11 +11,11 @@ public class PosMachineTest {
         PosMachine posMachine = new PosMachine();
 
         String expected = "***<store earning no money>Receipt***\n" +
-                "Name: Coca-Cola, Quantity: 5, Unit price: 3 (yuan), Subtotal: 15 (yuan)\n" +
+                "Name: Coca-Cola, Quantity: 4, Unit price: 3 (yuan), Subtotal: 12 (yuan)\n" +
                 "Name: Sprite, Quantity: 2, Unit price: 3 (yuan), Subtotal: 6 (yuan)\n" +
-                "Name: Battery, Quantity: 1, Unit price: 2 (yuan), Subtotal: 2 (yuan)\n" +
+                "Name: Battery, Quantity: 3, Unit price: 2 (yuan), Subtotal: 6 (yuan)\n" +
                 "----------------------\n" +
-                "Total: 23 (yuan)\n" +
+                "Total: 24 (yuan)\n" +
                 "**********************";
 
         assertEquals(expected, posMachine.printReceipt(ItemDataLoader.loadBarcodes()));
